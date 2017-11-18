0dcf82edb31e2725bed60eb0ed8f00784d521610
Spencer Catron <33788021+SpencerCatron@users.noreply.github.com>
2017-11-18 15:57:33 -0500

    Update Version3.java

--- Version3/Version3.java
+++ Version3/Version3.java
@@ -4,16 +4,21 @@ import java.util.Scanner;
 public class Version3 {
 	public static void main ( String  [ ] args)
 	{
+		//gets input
 		Scanner kb = new Scanner(System.in);
 		System.out.println("enter an expression to propagate error for");
 		String input = kb.nextLine();
 		
+		//loop until the user wants to stop putting in values
 		while(!input.equalsIgnoreCase("quit"))
 		{
+			//calculates the answer
 			System.out.println(calculate.Calculate(input));
 			System.out.println("Enter another value to evaluate(quit to quit");
 			input = kb.nextLine();
 		}
+		
+		kb.close(); 
 	}
 	
 
